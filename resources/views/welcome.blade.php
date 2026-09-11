64e96c6be9a0790b2d752d8848505784e2b4e73b
MirMatus <miriamlozano1206@gmail.com>
2026-09-10 19:02:39 -0600

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,6 +13,7 @@
     <h2 style="color: #8968CD;">Integrantes:</h2>
     <ul style="font-size: 1.1rem; line-height: 2;">
         <li style="color: #BA70C4;">Hernández González Karen Nayeli.</li>
+        <li style="color: #B5BEF5;">Lozano Matus Miriam Doyel.</li>
         <li style="color: #A8C9E1;">Paola Melissa Méndez Jiménez.</li>
     </ul>
 
