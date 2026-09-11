21555bc8cb32278f7ab7e0f77234a843b2fae1b1
knayequeen <karennayelihg@gmail.com>
2026-09-10 18:57:59 -0600

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,7 +13,8 @@
     
     <h2 style="color: #8968CD;">Integrantes:</h2>
     <ul style="font-size: 1.1rem; line-height: 2;">
-        <li style="color: ##A8C9E1;">Paola Melissa Méndez Jiménez.</li>
+        <li style="color: #A8C9E1;">Paola Melissa Méndez Jiménez.</li>
+        <li style="color: #BA70C4;">Hernández González Karen Nayeli.</li>
     </ul>
 
 </body>
