01d9a13179cb60b7cdfd057afafecc83fb85ada6
knayequeen <karennayelihg@gmail.com>
2026-09-10 18:59:41 -0600

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,13 +8,12 @@
     
     <h1 style="color: #743089;">Entrega 1 - Evaluación proyecto</h1>
     <p><strong>Materia:</strong> Implementación de sistemas.</p>
-    <p><strong>Profesora:</strong> Villegas Merlos Jessica.</p>
     <hr style="border: 0; border-top: 1px solid #e1bee7; margin: 20px 0;">
     
     <h2 style="color: #8968CD;">Integrantes:</h2>
     <ul style="font-size: 1.1rem; line-height: 2;">
-        <li style="color: #A8C9E1;">Paola Melissa Méndez Jiménez.</li>
         <li style="color: #BA70C4;">Hernández González Karen Nayeli.</li>
+        <li style="color: #A8C9E1;">Paola Melissa Méndez Jiménez.</li>
     </ul>
 
 </body>
